5f819163cacb380563df4e01b6a601d16645780b
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-06 11:26:02 +0200

    Navbar + styling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./creoshift_logo.jpg";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt="Creoshift logo" width={70} placeholder="blur" />
       <h1>Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
