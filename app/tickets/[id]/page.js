b3eed494a03ddd8b9d28b4f501c24c856e8fea88
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-08 14:20:01 +0200

    removing fetch for one

--- app/tickets/[id]/page.js
+++ app/tickets/[id]/page.js
@@ -1,49 +1,51 @@
 import { notFound } from "next/navigation";
 
-export const dynamicParams = true;
+// export const dynamicParams = true;
 
-export async function generateStaticParams() {
-  const res = await fetch("http://localhost:4000/tickets");
+// export async function generateStaticParams() {
+//   const res = await fetch("http://localhost:4000/tickets");
 
-  const tickets = await res.json();
+//   const tickets = await res.json();
 
-  return tickets.map((ticket) => ({
-    id: ticket.id,
-  }));
-}
+//   return tickets.map((ticket) => ({
+//     id: ticket.id,
+//   }));
+// }
 
 async function getTicket(id) {
   // //imitate delay to test loading screen
   // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  const res = await fetch("http://localhost:4000/tickets/" + id, {
-    next: {
-      revalidate: 60,
-    },
-  });
+  // const res = await fetch("http://localhost:4000/tickets/" + id, {
+  //   next: {
+  //     revalidate: 60,
+  //   },
+  // });
 
-  if (!res.ok) {
-    notFound();
-  }
+  // if (!res.ok) {
+  //   notFound();
+  // }
 
-  return res.json();
+  // return res.json();
+  return null;
 }
 
 export default async function TicketDetails({ params }) {
-  const ticket = await getTicket(params.id);
+  // const ticket = await getTicket(params.id);
   return (
-    <main>
-      <nav>
-        <h2>Ticket Details</h2>
-      </nav>
-      <div className="card">
-        <h3>{ticket.title}</h3>
-        <small>Created by {ticket.user_email}</small>
-        <p>{ticket.body}</p>
-        <div className={`pill ${ticket.priority}`}>
-          {ticket.priority} priority
-        </div>
-      </div>
-    </main>
+    <></>
+    // <main>
+    //   <nav>
+    //     <h2>Ticket Details</h2>
+    //   </nav>
+    //   <div className="card">
+    //     <h3>{ticket.title}</h3>
+    //     <small>Created by {ticket.user_email}</small>
+    //     <p>{ticket.body}</p>
+    //     <div className={`pill ${ticket.priority}`}>
+    //       {ticket.priority} priority
+    //     </div>
+    //   </div>
+    // </main>
   );
 }
