428d5a3e905fdc6cc62e90abe812fea724c537dc
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-13 11:50:20 +0200

    Footer refining

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -8,7 +8,7 @@ import X from "./X.png";
 export default function Footer() {
   return (
     <footer>
-      <div class="max-w-5xl mx-auto border-t-2 border-gray-200 py-5 flex flex-row justify-between items-center flex-wrap">
+      <div class="max-w-5xl mx-auto border-t-2 border-gray-200 py-5 flex flex-row justify-between items-center flex-wrap gap-y-4">
         {/* <div class="max-w-5xl mx-auto border-t-2 border-gray-200 py-5 sm:flex sm:flex-row sm:justify-between sm:items-center sm:flex-wrap grid grid-cols-2 grid-rows-2"> */}
 
         <Link href="/">
@@ -17,26 +17,30 @@ export default function Footer() {
             alt="Creoshift logo"
             width={70}
             loading="eager"
-            className="hover:scale-110"
+            className="hover:opacity-75"
           />
         </Link>
         <div class="footer-links m-3 flex flex-col sm:flex-row flex-wrap sm:space-x-4">
-          <a href="#" class=" ml-auto   ">
-            Privacy Policy
-          </a>
-          <a href="#" class=" ml-auto ">
-            Terms of Service
-          </a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Service</a>
         </div>
-        <div class="social-icons flex mt-4 sm:mt-0">
-          <a href="https://www.facebook.com" target="_blank" class="mr-4">
-            <Image src={Facebook} alt="Facebook Icon" class="w-8" />
+        <div class="social-icons flex mt-4 gap-4 sm:mt-0">
+          <a href="https://www.facebook.com" target="_blank">
+            <Image
+              src={Facebook}
+              alt="Facebook Icon"
+              class="w-8 hover:opacity-75"
+            />
           </a>
-          <a href="https://www.x.com" target="_blank" class="mr-4">
-            <Image src={X} alt="X Icon" class="w-8" />
+          <a href="https://www.x.com" target="_blank">
+            <Image src={X} alt="X Icon" class="w-8 hover:opacity-75" />
           </a>
           <a href="https://www.instagram.com" target="_blank">
-            <Image src={Instagram} alt="Instagram Icon" class="w-8" />
+            <Image
+              src={Instagram}
+              alt="Instagram Icon"
+              class="w-8 hover:opacity-75"
+            />
           </a>
         </div>
       </div>
