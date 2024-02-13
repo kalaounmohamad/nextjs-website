bc2f505694d85c3e746f95ecd6f20b26e65177cb
kalaounmohamad <kalaounmohamad@hotmail.com>
2024-02-13 12:23:38 +0200

    Replaced nav with div on ticketsList

--- app/tickets/page.js
+++ app/tickets/page.js
@@ -6,12 +6,12 @@ import Link from "next/link";
 export default function Tickets() {
   return (
     <main>
-      <nav className="flex justify-between items-center">
+      <div className="flex justify-between items-center">
         <h2>Tickets</h2>
         <Link href="/tickets/create">
           <button className="btn-primary py-1 px-3 text-2xl">+</button>
         </Link>
-      </nav>
+      </div>
       {/* suspense is made to make a loading just for a particular part of the
       page */}
       <Suspense fallback={<Loading />}>
